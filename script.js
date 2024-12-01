31352d7ca16726de168f176873bfc56379ef5def
Madeleine Persson <madde161@gmail.com>
2024-12-01 19:47:38 +0100

    test igen ny

---
+++ script.js
@@ -0,0 +1,10 @@
+// Scare the nice users
+//Ger tom ruta man kan skriva i. Värdet returneras i konsolen
+ 
+let username = prompt("What is your username?");
+if (!username) {
+    username = "Boring person";
+}
+alert(`Boo ${username}`);
+
+// test
